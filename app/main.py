5e4c41325b6e5453a6eb391d42c5dc689aaf3d19
“Jorik” <georgiys777@gmail.com>
2023-11-17 13:03:48 +0100

    auth endpoint

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from app.api import api_router
 import sentry_sdk
 from app.settings import settings
 
+from app.auth import router_auth
+
 app = FastAPI(
     title=settings.APP_NAME,
     description='we are the champions',
@@ -14,6 +16,7 @@ app = FastAPI(
 sentry_sdk.init(dsn=settings.SENTRY_SDK_DSN, traces_sample_rate=1.0, profiles_sample_rate=1.0)
 
 app.include_router(api_router.router)
+app.include_router(router_auth.router)
 
 # TODO: temporary disable to check regular web endpoints. In future useful for REACT
 # app = VersionedFastAPI(app, version_format='{major}', prefix_format='/v{major}')
