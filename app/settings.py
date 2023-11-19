a4a880c79e4a1829a43dbd42158605c31c187569
“Jorik” <georgiys777@gmail.com>
2023-11-19 10:06:58 +0100

    dev-7 for vasyl

--- app/settings.py
+++ app/settings.py
@@ -6,17 +6,28 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+load_dotenv()
+
+
 class Settings(BaseSettings):
     APP_NAME: str = "Awesome API"
     SENTRY_SDK_DSN: str
     CURRENT_APP_VERSION: str = "0.1.0"
     DEBUG: bool = True
 
+    # DATABASE_NAME = os.getenv('DATABASE', '')
+    # DATABASE_USER = os.getenv('DATABASE', '')
+    # DATABASE_PASSWORD = os.getenv('DATABASE', '')
+    # DATABASE_HOST = os.getenv('DATABASE', '')
+    # DATABASE_PORT = os.getenv('DATABASE', '')
+
     # in order to place settings with the main file we dynamically get path to .env
     model_config = SettingsConfigDict(env_file=os.path.join(os.getcwd(), '.env'))
 
 
 settings = Settings()
+
+
 class Item(BaseModel):
     names: dict = {
         "name": "default_name",
