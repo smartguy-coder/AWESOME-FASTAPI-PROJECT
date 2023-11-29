da6e277c66310ad7a578aec4da64f3d00cfbef6d
Konstantin Nezhalsky <konstantin.nezhalsky@gmail.com>
2023-11-29 18:47:44 +0200

    removed APP_URL from Settings

--- app/settings.py
+++ app/settings.py
@@ -14,7 +14,6 @@ class Settings(BaseSettings):
     SENTRY_SDK_DSN: str
     CURRENT_APP_VERSION: str = "0.1.0"
     DEBUG: bool = True
-    APP_URL: str = "https://www.google.com/"
 
     # in order to place settings with the main file we dynamically get path to .env
     model_config = SettingsConfigDict(env_file=os.path.join(os.getcwd(), ".env"))
