4941e102f9da6e81f757e56c443e65c21d0099c2
Konstantin Nezhalsky <konstantin.nezhalsky@gmail.com>
2023-11-29 18:36:56 +0200

    added settings caching

--- app/settings.py
+++ app/settings.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-
+from functools import lru_cache
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
@@ -14,12 +14,18 @@ class Settings(BaseSettings):
     SENTRY_SDK_DSN: str
     CURRENT_APP_VERSION: str = "0.1.0"
     DEBUG: bool = True
+    APP_URL: str = "https://www.google.com/"
 
     # in order to place settings with the main file we dynamically get path to .env
     model_config = SettingsConfigDict(env_file=os.path.join(os.getcwd(), ".env"))
 
 
-settings = Settings()
+@lru_cache
+def get_settings():
+    return Settings()
+
+
+settings = get_settings()
 
 
 class Item(BaseModel):
