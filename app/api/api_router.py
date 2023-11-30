40fd48e181d88a67848a7a34a045a4d5a3e1b269
Konstantin Nezhalsky <konstantin.nezhalsky@gmail.com>
2023-11-30 09:15:49 +0200

    fixed api_router.py

--- app/api/api_router.py
+++ app/api/api_router.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter
 from fastapi_versioning import version
 
-from app.settings import Item
-
 router = APIRouter(
     prefix="/api",
     tags=["landing"],
@@ -13,6 +11,4 @@ router = APIRouter(
 @router.get("/about")
 @version(1)
 async def about() -> dict:
-    item = Item()
-    item_dict = item.to_dict()
-    return item_dict
+    return {"message": "Hello, world!"}
