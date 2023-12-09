39630d273eff323bc34acc13e84cfe4d012ce0fa
Василь Картичак <w.i.k.mailua@gmail.com>
2023-12-09 17:45:09 +0200

    on code review

--- app/settings.py
+++ app/settings.py
@@ -18,12 +18,15 @@ class Settings(BaseSettings):
     MONGODB_USER: str
     MONGODB_PASSWORD: str
     MONGODB_URI: str
+    MONGODB_SERVER: str
     MONGODB_DB_STORIES: str = "stories"
     MONGODB_COLLECTION_STORIES: str = "user_stories"
 
     @property
     def MONGODB_URI_FINAL(self) -> str:
-        return self.MONGODB_URI.format(mongodb_user=self.MONGODB_USER, mongodb_password=self.MONGODB_PASSWORD)
+        return self.MONGODB_URI.format(
+            mongodb_user=self.MONGODB_USER, mongodb_password=self.MONGODB_PASSWORD, mongodb_server=self.MONGODB_SERVER,
+        )
 
     # in order to place settings with the main file we dynamically get path to .env
     model_config = SettingsConfigDict(env_file=os.path.join(os.getcwd(), ".env"))
